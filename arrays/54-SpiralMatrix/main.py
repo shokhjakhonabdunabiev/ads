a8073924a29619ef1e24afce0be5909890ff7774
shokhjakhonabdunabiev <20shoxjaxon02@gmail.com>
2025-11-22 22:08:35 +0100

    54. Spiral Matrix - Additional solutions added

--- arrays/54-SpiralMatrix/main.py
+++ arrays/54-SpiralMatrix/main.py
@@ -54,12 +54,77 @@ def spiralOrder(matrix: list[list[int]]) -> list[int]:
 
     return res
 
+def spiralOrder2(matrix: list[list[int]]) -> int:
+    if len(matrix) == 0:
+        return []
+    
+    res = []
+    rows, cols = len(matrix), len(matrix[0])
+    direction = 1
+    i, j = 0, -1
+
+    while rows * cols > 0:
+        for _ in range(cols):
+            j += direction
+            res.append(matrix[i][j])
+        rows -= 1
+        for _ in range(rows):
+            i += direction
+            res.append(matrix[i][j])
+        cols -= 1
+        direction *= -1
+    return res
+
+# Constrains mentions that -100 <= matrix[i][j] <= 100
+# Space Complexity: might require additonal space to track visited elements,
+# if we cant modift matrix in-place
+def spiralOrder3(matrix: list[list[int]]) -> int:
+    if len(matrix) == 0:
+        return []
+    res = []
+
+    VISITED = 101
+    directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    current_direction = 0
+    change_direction = 0
+    rows, cols = len(matrix), len(matrix[0])
+    row, col = 0, 0
+    res.append(matrix[0][0])
+    matrix[0][0] = VISITED
+
+    while change_direction < 2:
+        while True:
+            next_row = row + directions[current_direction][0]
+            next_col = col + directions[current_direction][1]
+
+            if not (0 <= next_row < rows and 0 <= next_col < cols):
+                break
+            if matrix[next_row][next_col] == VISITED:
+                break
+            
+            change_direction = 0
+            row, col = next_row, next_col
+            res.append(matrix[row][col])
+            matrix[row][col] = VISITED
+        
+        current_direction = (current_direction + 1) % 4
+        change_direction += 1
+    return res
+
 if __name__ == '__main__':
-    matrix = [
-        [1,2,3],
-        [4,5,6],
-        [7,8,9],
+    test_cases = [
+        [
+            [1,2,3],
+            [4,5,6],
+            [7,8,9],
+        ],
+        [
+            [1,2,3,4,5,6],
+        ],
+        []
     ]
 
-    res = spiralOrder(matrix)
-    print(res)
+    for i, test_case in enumerate(test_cases):
+        res = spiralOrder3(test_case)
+        print(f"Case {i + 1}:", res)
+

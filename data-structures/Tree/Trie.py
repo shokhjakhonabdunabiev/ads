c4ff82ad39a27e8bbd2cc52884970d7c35b1e294
shokhjakhonabdunabiev <20shoxjaxon02@gmail.com>
2025-11-26 13:57:44 +0100

    Trie implemented

--- data-structures/Tree/Trie.py
+++ data-structures/Tree/Trie.py
@@ -2,5 +2,44 @@
 Implement Trie
 '''
 
+class TrieNode:
+    def __init__(self):
+        self.children = dict()
+        self.is_word = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for ch in word:
+            if ch not in curr.children:
+                curr.children[ch] = TrieNode()
+            curr = curr.children[ch]
+        curr.is_word = True
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+        for ch in word:
+            if ch not in curr.children:
+                return False
+            curr = curr.children[ch]
+        return curr.is_word
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+        for ch in prefix:
+            if ch not in curr.children:
+                return False
+            curr = curr.children[ch]
+        return True
+
 if __name__ == "__main__":
-    pass
+    trie = Trie()
+    trie.insert("hello")
+    print(trie.search("hello"))
+    print(trie.search("hella"))
+    print(trie.startsWith("he"))
+    print(trie.startsWith("ab"))
+    
